e8ed488951f5436f0ba98318db1cd57eaa7eb48a
mycode01@hotmail.com <mycode01@hotmail.com>
2022-09-28 15:23:27 +0900

    baseresponses no need primary constructor

--- src/main/java/com/justcodeit/moyeo/study/BaseResponse.java
+++ src/main/java/com/justcodeit/moyeo/study/BaseResponse.java
@@ -4,12 +4,8 @@ package com.justcodeit.moyeo.study;
 public class BaseResponse {
 
   int code;
-
   public BaseResponse(int code) {
     this.code = code;
-  }
-
-  public BaseResponse() {
   }
 
   public int getCode() {
